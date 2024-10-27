bdc5342650231e95aac658fa919f5c4dc3b411f4
Damián Nohales <damian.nohales@rover.com>
2024-10-27 16:27:56 -0300

    Add multi OS support in Lua script

--- lua/shortcutviewer.lua
+++ lua/shortcutviewer.lua
@@ -3,10 +3,41 @@ local options = require 'mp.options'
 local utils = require 'mp.utils'
 local input = require 'mp.input'
 
-local command = '/home/dnohales/Dummy/mpv_shortcutviewer/build/linux/x64/release/bundle/mpv_shortcutviewer'
+local function detect_os()
+    local binary_format = package.cpath:match("%p[\\|/]?%p(%a+)")
+    if binary_format == "dll" then
+        return "windows"
+    elseif binary_format == "so" then
+        return "linux"
+    elseif binary_format == "dylib" then
+        return "macos"
+    end
+    return nil
+end
+
+local function get_command_path()
+    local os = detect_os()
+    if os == nil then
+        return nil
+    end
+
+    local command = utils.join_path(mp.get_script_directory(), os)
+    command = utils.join_path(command, "mpv_shortcutviewer")
+    if os == "windows" then
+        command = command .. ".exe"
+    end
+
+    return command
+end
 
 
 function shortcutviewer()
+    local command_path = get_command_path()
+    if command_path == nil then
+        mp.osd_message("[shortcutviewer] Unable to detect your OS")
+        return
+    end
+
     local bindings = mp.get_property_native("input-bindings", {})
     local active = {}
     for _, bind in pairs(bindings) do
@@ -29,7 +60,7 @@ function shortcutviewer()
 
     mp.command_native({
         name = 'subprocess',
-        args = command,
+        args = command_path,
         playback_only = true,
         stdin_data = utils.format_json(active_as_list),
     })
